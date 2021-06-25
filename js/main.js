3dd001eb23084d8a11a2cb76042bc3c2eccfa3a9
Elena9477 <elena9477@yandex.ru>
2021-06-25 13:57:29 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -2,25 +2,25 @@ function getCoordsInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min === max) {
-    return Math.abs(min);
+    return +Math.abs(min);
   }
   if (max > min) {
-    return Math.abs(Math.floor(Math.random() * (max - min + 1) + min));
+    return +Math.abs(Math.floor(Math.random() * (max - min + 1) + min));
   } else {
-    return Math.abs(Math.floor(Math.random() * (min - max + 1) + max));
+    return +Math.abs(Math.floor(Math.random() * (min - max + 1) + max));
   }
 }
 
 function getCoordsFloat(min, max, number) {
   if (min === max) {
-    return Math.abs(min.toFixed(number));
+    return +Math.abs(min.toFixed(number));
   }
   if (max > min) {
-    return Math.abs(Math.random() * (max - min + 1) + min).toFixed(number);
+    return +Math.abs(Math.random() * (max - min) + min).toFixed(number);
   } else {
-    return Math.abs(Math.random() * (min - max + 1) + max).toFixed(number);
+    return +Math.abs(Math.random() * (min - max) + max).toFixed(number);
   }
 }
 
 getCoordsInt(1, 5);
-getCoordsFloat(0, 20, 3);
+getCoordsFloat(1.2, 1.3, 3);
