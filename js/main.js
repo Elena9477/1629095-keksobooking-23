01b1c066079702a0e53509abb328c0ce527805c1
Elena9477 <elena9477@yandex.ru>
2021-07-01 19:42:15 +0300

    Правки модуль 4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+const MINLAT = 35.65000;
+const MAXLAT = 35.70000;
+const FIXTONUMBER = 5;
+const MINLNG = 139.70000;
+const MAXLNG =  139.80000;
+
+const MINPRICE = 1;
+const MAXPRICE = 1000;
+
+const MINROOMS = 1;
+const MAXROOMS = 100;
+
+const MINGUESTS = 1;
+const MAXGUESTS = 200;
+
+const TIMES = ['12:00', '13:00', '14:00'];
+const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = [
+  'Новый дом, высокие потолки, удобная планировка - набор превосходных характеристик',
+  'Все необходимые для бизнеса коммуникации, хороший подъезд и отсутствие соседей делают помещение идеальным местом для локации предприятия!',
+  'В новой квартире простор сочетается с уютом, универсальность - с функциональностью, без этого в современном жилье никак не обойтись. А парк рядом с домом - просто мечта!',
+  'Функциональность помещения позволяет осуществлять все задумки босса - производственная часть, зона отдыха и кухня - есть всё что нужно в работе фирмы.',
+  'Делать бизнес в таком помещении не только важно, но и приятно!',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+const MINPHOTOSNUMBER = 1;
+const MAXPHOTOSNUMBER = 20;
+
+
 function getCoordsInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -22,61 +57,36 @@ function getCoordsFloat(min, max, number) {
   }
 }
 
-function createArr(ar, obj) {
-  return ar.push(obj);
-}
+const getType = () => TYPES[getCoordsInt(0, TYPES.length - 1)];
 
-function getType() {
-  const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  return types[getCoordsInt(0, 4)];
-}
-
-function getTime() {
-  const times = ['12:00', '13:00', '14:00'];
-  return times[getCoordsInt(0, times.length - 1)];
-}
+const getTime = () => TIMES[getCoordsInt(0, TIMES.length - 1)];
 
 function getFeatures() {
-  const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  const featuresNumber = getCoordsInt(0, features.length - 1);
-  const copy = features.slice();
+  const featuresNumber = getCoordsInt(0, FEATURES.length - 1);
+  const copy = FEATURES.slice();
   let feature = '';
   const checkedFeatures = [];
   for(let i = 0; i < featuresNumber; i++) {
     feature = copy.splice(getCoordsInt(0, copy.length - 1), 1);
-    checkedFeatures.push(feature);
+    checkedFeatures.push(...feature);
   }
   return checkedFeatures;
 }
 
-function getDescr() {
-  const descriptions = [
-    'Новый дом, высокие потолки, удобная планировка - набор превосходных характеристик',
-    'Все необходимые для бизнеса коммуникации, хороший подъезд и отсутствие соседей делают помещение идеальным местом для локации предприятия!',
-    'В новой квартире простор сочетается с уютом, универсальность - с функциональностью, без этого в современном жилье никак не обойтись. А парк рядом с домом - просто мечта!',
-    'Функциональность помещения позволяет осуществлять все задумки босса - производственная часть, зона отдыха и кухня - есть всё что нужно в работе фирмы.',
-    'Делать бизнес в таком помещении не только важно, но и приятно!',
-  ];
-  return descriptions[getCoordsInt(0, descriptions.length - 1)];
-}
+const getDescr = () => DESCRIPTIONS[getCoordsInt(0, DESCRIPTIONS.length - 1)];
 
 function getPhotos() {
-  const photos = [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ];
-  const photosNumber = getCoordsInt(1, 20);
+  const photosNumber = getCoordsInt(MINPHOTOSNUMBER, MAXPHOTOSNUMBER);
   const photosList = [];
   for(let i = 0; i < photosNumber; i++) {
-    photosList.push(photos[getCoordsInt(0, photos.length - 1)]);
+    photosList.push(PHOTOS[getCoordsInt(0, PHOTOS.length - 1)]);
   }
   return photosList;
 }
 
 function getObj(i) {
-  const locationLat = getCoordsFloat(35.65000, 35.70000, 5);
-  const locationLng = getCoordsFloat(139.70000, 139.80000, 5);
+  const locationLat = getCoordsFloat(MINLAT, MAXLAT, FIXTONUMBER);
+  const locationLng = getCoordsFloat(MINLNG, MAXLNG, FIXTONUMBER);
   const obj = {
     author: {
       avatar: `img/avatars/user${(i < 10) ? `0${i}` : i}.png`,
@@ -84,10 +94,10 @@ function getObj(i) {
     offer: {
       title: 'Дом моей мечты',
       address: `${locationLat}, ${locationLng}`,
-      price: getCoordsInt(1, 1000),
+      price: getCoordsInt(MINPRICE, MAXPRICE),
       type: getType(),
-      rooms: getCoordsInt(1, 100),
-      guests: getCoordsInt(1, 200),
+      rooms: getCoordsInt(MINROOMS, MAXROOMS),
+      guests: getCoordsInt(MINGUESTS, MAXGUESTS),
       checkin: getTime(),
       checkout: getTime(),
       features: getFeatures(),
@@ -102,10 +112,13 @@ function getObj(i) {
   return obj;
 }
 
-const arr = [];
+const arr = () => {
+  const array = [];
+  for(let i = 0; i < 10; i++) {
+    array.push(getObj(i+1));
+  }
+  return array;
+};
 
-for(let i = 0; i < 10; i++) {
-  createArr(arr, getObj(i+1));
-}
-
+arr();
 
