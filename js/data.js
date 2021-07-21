536f3dd7a1787ceb4601e75b76bf033ffdf1b555
Elena9477 <elena9477@yandex.ru>
2021-07-21 16:31:54 +0300

    Правки module8-task1

--- js/data.js
+++ js/data.js
@@ -1,9 +1,9 @@
 const TYPES = {
-  flat: "Квартира",
-  bungalow: "Бунгало",
-  house: "Дом",
-  palace: "Дворец",
-  hotel: "Отель",
+  flat: 'Квартира',
+  bungalow: 'Бунгало',
+  house: 'Дом',
+  palace: 'Дворец',
+  hotel: 'Отель',
 };
 
 const MINPRICES = {
